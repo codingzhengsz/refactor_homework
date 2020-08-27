0e3d917a6ffb125e1e5f37c0bfe86e80d186aaac
Martin <codingzhengsz@163.com>
2020-08-27 20:59:35 +0800

    [Rank][Martin] refactor: extract a class to resolve basic logic

--- src/rank.js
+++ src/rank.js
@@ -1,101 +1,87 @@
-const history = [
-    {
-        zone: 'east-indies',
-        profit: 5,
-    }, {
-        zone: 'west-indies',
-        profit: 15,
-    }, {
-        zone: 'china',
-        profit: -2,
-    }, {
-        zone: 'west-africa',
-        profit: 7,
-    },
-];
-
-const voyage = {
-    zone: 'west-indies',
-    length: 10,
-};
-
-
-
-function voyageRisk(voyage) {
-    let result = 1;
-    if (voyage.length > 4) {
-        result += 2;
-    }
-    if (voyage.length > 8) {
-        result += voyage.length - 8;
-    }
-    if ([
-        'china',
-        'east-indies',
-    ].includes(voyage.zone)) {
-        result += 4;
-    }
-    return Math.max(result, 0);
-}
-
-function hasChina(history) {
-    return history.some(v => 'china' === v.zone);
-}
-
-function captainHistoryRisk(voyage, history) {
-    let result = 1;
-    if (history.length < 5) {
-        result += 4;
-    }
-    result += history.filter(v => v.profit < 0).length;
-    if (voyage.zone === 'china' && hasChina(history)) {
-        result -= 2;
-    }
-    return Math.max(result, 0);
-}
-
-function voyageProfitFactor(voyage, history) {
-    let result = 2;
-    if (voyage.zone === 'china') {
-        result += 1;
-    }
-    if (voyage.zone === 'east-indies') {
-        result += 1;
-    }
-    if (voyage.zone === 'china' && hasChina(history)) {
-        result += 3;
-        if (history.length > 10) {
-            result += 1;
-        }
-        if (voyage.length > 12) {
-            result += 1;
-        }
-        if (voyage.length > 18) {
-            result -= 1;
-        }
-    } else {
-        if (history.length > 8) {
-            result += 1;
-        }
-        if (voyage.length > 14) {
-            result -= 1;
-        }
-    }
-    return result;
-}
-
 function rating(voyage, history) {
-    const vpf = voyageProfitFactor(voyage, history);
-    const vr = voyageRisk(voyage);
-    const chr = captainHistoryRisk(voyage, history);
-    if (vpf * 3 > (vr + chr * 2)) {
-        return 'A';
-    } else {
-        return 'B';
+    return new Rating(voyage, history).result;
+}
+
+class Rating {
+    constructor(voyage, history) {
+        this.voyage = voyage;
+        this.history = history;
+    }
+
+    get result() {
+        const vpf = this.voyageProfitFactor;
+        const vr = this.voyageRisk;
+        const chr = this.captainHistoryRisk;
+        if (vpf * 3 > (vr + chr * 2)) {
+            return 'A';
+        } else {
+            return 'B';
+        }
+    }
+
+    get voyageRisk() {
+        let result = 1;
+        if (this.voyage.length > 4) {
+            result += 2;
+        }
+        if (this.voyage.length > 8) {
+            result += this.voyage.length - 8;
+        }
+        if ([
+            'china',
+            'east-indies',
+        ].includes(this.voyage.zone)) {
+            result += 4;
+        }
+        return Math.max(result, 0);
+    }
+
+    get hasChina() {
+        return this.history.some(v => 'china' === v.zone);
+    }
+
+    get captainHistoryRisk() {
+        let result = 1;
+        if (this.history.length < 5) {
+            result += 4;
+        }
+        result += this.history.filter(v => v.profit < 0).length;
+        if (this.voyage.zone === 'china' && this.hasChina) {
+            result -= 2;
+        }
+        return Math.max(result, 0);
+    }
+
+    get voyageProfitFactor() {
+        let result = 2;
+        if (this.voyage.zone === 'china') {
+            result += 1;
+        }
+        if (this.voyage.zone === 'east-indies') {
+            result += 1;
+        }
+        if (this.voyage.zone === 'china' && this.hasChina) {
+            result += 3;
+            if (this.history.length > 10) {
+                result += 1;
+            }
+            if (this.voyage.length > 12) {
+                result += 1;
+            }
+            if (this.voyage.length > 18) {
+                result -= 1;
+            }
+        } else {
+            if (this.history.length > 8) {
+                result += 1;
+            }
+            if (this.voyage.length > 14) {
+                result -= 1;
+            }
+        }
+        return result;
     }
 }
 
-module.exports = {};
+module.exports = {rating};
 
-const myRating = rating(voyage, history);
-console.log(`myRating: ${myRating}`);
