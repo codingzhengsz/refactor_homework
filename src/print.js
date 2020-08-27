fa816f1f12438485a47cd8f83b1ce3ad66c2a06a
Martin <codingzhengsz@163.com>
2020-08-27 21:51:12 +0800

    [Print][Martin] refactor: extract a method to recordDueDate

--- src/print.js
+++ src/print.js
@@ -1,30 +1,31 @@
 function printHeader() {
-  console.log('***********************');
-  console.log('**** Customer Owes ****');
-  console.log('***********************');
+    console.log('***********************');
+    console.log('**** Customer Owes ****');
+    console.log('***********************');
 }
 
 function printDetails(invoice, outstanding) {
-  console.log(`name: ${invoice.customer}`);
-  console.log(`amount: ${outstanding}`);
-  console.log(`amount: ${invoice.dueDate.toLocaleDateString()}`);
+    console.log(`name: ${invoice.customer}`);
+    console.log(`amount: ${outstanding}`);
+    console.log(`amount: ${invoice.dueDate.toLocaleDateString()}`);
 }
 
 function calculateOurStanding(invoice) {
-  let outstanding = 0;
-  for (const o of invoice.borderSpacing) {
-    outstanding += o.amount;
-  }
-  return outstanding;
+    let outstanding = 0;
+    for (const o of invoice.borderSpacing) {
+        outstanding += o.amount;
+    }
+    return outstanding;
 }
 
-function printOwing (invoice) {
-  printHeader();
-  let outstanding = calculateOurStanding(invoice);
+function recordDueDate(invoice) {
+    const today = new Date();
+    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+}
 
-  // record due date
-  const today = new Date();
-  invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
-
-  printDetails(invoice, outstanding);
+function printOwing(invoice) {
+    printHeader();
+    let outstanding = calculateOurStanding(invoice);
+    recordDueDate(invoice);
+    printDetails(invoice, outstanding);
 }
